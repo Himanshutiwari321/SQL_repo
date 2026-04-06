5d01276a92346f76a718bd5f756cf3e14bf3ecc9
HimanshuTiwari321 <himanshu546686@gmail.com>
2026-04-06 23:27:48 +0530

    add

--- SQL Q3.sql
+++ SQL Q3.sql
@@ -1,14 +1,9 @@
-#display duplicate of a column 
-SELECT Department, count(*) FROM employeetabel
-GROUP BY Department 
-HAVING count(*)>1;
-
-SELECT *
-FROM employeetabel
-WHERE Department IN ( SELECT Department FROM employeetabel 
+#Display duplicates of a column.
+#simple apporoch 
+SELECT  Department ,COUNT(*) FROM employeetabel
 GROUP BY Department
-HAVING COUNT(*) > 1
-);
+HAVING COUNT(*) > 1;
 
-SELECT * FROM ( SELECT *, COUNT(*) OVER (PARTITION BY Department) AS cnt FROM employeetabel) t
+#apporoch window function 
+SELECT * FROM (SELECT *, COUNT(*) OVER (PARTITION BY Department) AS cnt FROM employeetabel ) t 
 WHERE cnt > 1;
